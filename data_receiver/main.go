f86053812892eb154b248bd3515df3ef8d326070
a.dinvarzadeh <mdinvarzade@gmail.com>
2025-10-05 23:15:01 +0330

    feat: setup kafka

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	types "tolling/Types"
 
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gorilla/websocket"
 )
 
+var kafkaTopic = "obuData"
+
 func main() {
-	reveiver := NewDataReceiver()
-	http.HandleFunc("/ws", reveiver.HandleWS)
+	receiver, err := NewDataReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/ws", receiver.HandleWS)
 	http.ListenAndServe(":3000", nil)
 	fmt.Println("data receiver work fine ")
 }
@@ -19,6 +26,7 @@ func main() {
 type DataReceiver struct {
 	messageChan chan types.OBUData
 	conn        *websocket.Conn
+	prod        *kafka.Producer
 }
 
 func (dr *DataReceiver) HandleWS(w http.ResponseWriter, r *http.Request) {
@@ -35,10 +43,42 @@ func (dr *DataReceiver) HandleWS(w http.ResponseWriter, r *http.Request) {
 	go dr.weReceiveLoop()
 }
 
-func NewDataReceiver() *DataReceiver {
+func (dr *DataReceiver) ProduceData(data types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	err = dr.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &kafkaTopic, Partition: kafka.PartitionAny},
+		Value:          b,
+	}, nil)
+	return err
+}
+
+func NewDataReceiver() (*DataReceiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
+	}
+
+	// start another go routin to check if we have delivered data
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
 	return &DataReceiver{
 		messageChan: make(chan types.OBUData, 1028),
-	}
+		prod:        p,
+	}, nil
 }
 
 func (dr *DataReceiver) weReceiveLoop() {
@@ -50,6 +90,8 @@ func (dr *DataReceiver) weReceiveLoop() {
 			continue
 		}
 		fmt.Printf("received OBU data from %d :: lat %.2f and lon %.2f \n", data.OBUID, data.Lat, data.Lon)
-		dr.messageChan <- data
+		if err := dr.ProduceData(data); err != nil {
+			fmt.Println("Kafka produce error ", err)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module tolling
 
-go 1.25.1
+go 1.24.6
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require github.com/gorilla/websocket v1.5.3
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.11.1 // indirect
